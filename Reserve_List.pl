97573d99c69efac62c329692203ac861c66d2e49
dimitrismatsanganis <jimmatsa@hotmail.gr>
2020-03-21 19:29:55 +0200

    Update Reserve_List.pl

--- Reserve_List.pl
+++ Reserve_List.pl
@@ -7,4 +7,6 @@ reverse([], Reversed, Reversed).  % An empty list is already reversed.
 reverse([Head|Tail], SoFar, Reversed) :-
                     reverse(Tail, [Head|SoFar], Reversed).
 % Every time "put" the list's head to another list (SoFar list) and continue the process
-% without the list's head until the list become empty.         
+% without the list's head until the list become empty.    
+
+% Note the difference between reverse/2 and reverse/3.
