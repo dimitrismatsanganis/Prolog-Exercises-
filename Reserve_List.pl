2a78b69453cd2f61474784109b0d04b0199e1c10
dimitrismatsanganis <jimmatsa@hotmail.gr>
2020-03-21 19:25:58 +0200

    Update Reserve_List.pl

--- Reserve_List.pl
+++ Reserve_List.pl
@@ -1,6 +1,8 @@
 reverse(List, Reversed) :-
           reverse(List, [], Reversed).
-
+% Initialize another rule with 3 objects.
 reverse([], Reversed, Reversed).
+% An empty list is already reversed.
 reverse([Head|Tail], SoFar, Reversed) :-
           reverse(Tail, [Head|SoFar], Reversed).
+% Every time "put" the list's head to another list (SoFar list) and continue the process without the list's head until the list become empty.         
