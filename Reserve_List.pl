ba8db8ff75923eebdeec2b249bc40d266bc99977
dimitrismatsanganis <jimmatsa@hotmail.gr>
2020-03-21 19:32:50 +0200

    Reversing a list.

--- Reserve_List.pl
+++ Reserve_List.pl
@@ -7,6 +7,8 @@ reverse([], Reversed, Reversed).  % An empty list is already reversed.
 reverse([Head|Tail], SoFar, Reversed) :-
                     reverse(Tail, [Head|SoFar], Reversed).
 % Every time "put" the list's head to another list (SoFar list) and continue the process
-% without the list's head until the list become empty.    
+% without the list's head until the list become empty.
 
+%-------------------------------------------------------
 % Note the difference between reverse/2 and reverse/3.
+%-------------------------------------------------------
