6155187dca330c01545d34902117333c5e92bf99
dimitrismatsanganis <jimmatsa@hotmail.gr>
2020-03-23 00:59:47 +0200

    Update Remove_Nth_element.pl

--- Remove_Nth_element.pl
+++ Remove_Nth_element.pl
@@ -1,9 +1,9 @@
-remove_at(X,[X|Xs],1,Xs).
+remove(X,[X|Xs],1,Xs).
 % The first element in the list is number 1.
 
-remove_at(X,[Y|Xs],N,[Y|Ys]) :- N > 1, 
-                                N1 is N - 1, remove_at(X,Xs,N1,Ys). 
+remove(X,[Y|Xs],N,[Y|Ys]) :- N > 1, 
+                                N1 is N - 1, remove(X,Xs,N1,Ys). 
                                 
-% Explanation of the rule remove_at/4;
+% Explanation of the rule remove/4;
 % X is the Nth element of the list L.
 % R is the list that remains when the Nth element is removed from L.
