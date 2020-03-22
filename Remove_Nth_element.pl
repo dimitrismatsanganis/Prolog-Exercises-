3d23825e12d7e48dca690db8d167d497239fa0a7
dimitrismatsanganis <jimmatsa@hotmail.gr>
2020-03-23 00:57:27 +0200

    Update Remove_Nth_element.pl

--- Remove_Nth_element.pl
+++ Remove_Nth_element.pl
@@ -3,6 +3,7 @@ remove_at(X,[X|Xs],1,Xs).
 
 remove_at(X,[Y|Xs],N,[Y|Ys]) :- N > 1, 
                                 N1 is N - 1, remove_at(X,Xs,N1,Ys). 
-% remove_at(X,L,N,R) 
+                                
+% Explanation of the rule remove_at/4;
 % X is the Nth element of the list L
 % R is the list that remains when the Nth element is removed from L.
