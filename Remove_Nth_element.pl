2fa426f2feb5e0f7521dbd78420aadafd021e00d
dimitrismatsanganis <jimmatsa@hotmail.gr>
2020-03-23 00:55:53 +0200

    Fix code's comments vol2.

--- Remove_Nth_element.pl
+++ Remove_Nth_element.pl
@@ -4,5 +4,5 @@ remove_at(X,[X|Xs],1,Xs).
 remove_at(X,[Y|Xs],N,[Y|Ys]) :- N > 1, 
                                 N1 is N - 1, remove_at(X,Xs,N1,Ys). 
 % remove_at(X,L,N,R) 
-%X is the Nth element of the list L
-%R is the list that remains when the Nth element is removed from L.
+% X is the Nth element of the list L
+% R is the list that remains when the Nth element is removed from L.
