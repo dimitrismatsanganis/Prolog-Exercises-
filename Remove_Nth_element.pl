82e23283f0ba2729c3a931deb23d19fa2376acba
dimitrismatsanganis <jimmatsa@hotmail.gr>
2020-03-23 01:01:15 +0200

    Remove the Nth element from a list.
    Remove the Nth element from a list.
    ?- remove(X,[a,A,b,2,3,c,d],3,R).
    X = b
    R = [a,A,2,3,c,d]

--- Remove_Nth_element.pl
+++ Remove_Nth_element.pl
@@ -2,7 +2,7 @@ remove(X,[X|Xs],1,Xs).
 % The first element in the list is number 1.
 
 remove(X,[Y|Xs],N,[Y|Ys]) :- N > 1, 
-                                N1 is N - 1, remove(X,Xs,N1,Ys). 
+                             N1 is N - 1, remove(X,Xs,N1,Ys). 
                                 
 % Explanation of the rule remove/4;
 % X is the Nth element of the list L.
