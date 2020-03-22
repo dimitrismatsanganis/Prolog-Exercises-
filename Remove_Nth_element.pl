c1e3e42fbc585dc405e6589b41b185b6a5a78c12
dimitrismatsanganis <jimmatsa@hotmail.gr>
2020-03-23 00:57:52 +0200

    Remove the Nth element from a list.
    Remove the Nth element from a list.
    ?- remove_at(X,[a,A,b,2,3,c,d],3,R).
    X = b
    R = [a,A,2,3,c,d]

--- Remove_Nth_element.pl
+++ Remove_Nth_element.pl
@@ -5,5 +5,5 @@ remove_at(X,[Y|Xs],N,[Y|Ys]) :- N > 1,
                                 N1 is N - 1, remove_at(X,Xs,N1,Ys). 
                                 
 % Explanation of the rule remove_at/4;
-% X is the Nth element of the list L
+% X is the Nth element of the list L.
 % R is the list that remains when the Nth element is removed from L.
