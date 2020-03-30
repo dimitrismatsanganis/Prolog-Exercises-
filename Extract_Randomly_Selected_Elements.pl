74b57b042f5e9b75518d8eba181a5186ec274c37
dimitrismatsanganis <jimmatsa@hotmail.gr>
2020-03-31 01:03:19 +0300

    Update Extract_Randomly_Selected_Elements.pl

--- Extract_Randomly_Selected_Elements.pl
+++ Extract_Randomly_Selected_Elements.pl
@@ -1,8 +1,10 @@
 remove(X,[X|Xs],1,Xs).
-
+% 
 remove(X,[Y|Xs],N,[Y|Ys]) :- N > 1, 
                              N1 is N - 1, remove(X,Xs,N1,Ys). 
 randomly_select(_,0,[]).
+% randomly_select(L,N,R), the list R contains N randomly selected 
+% items taken from the list L.
 
 randomly_select(Xs,N,[X|Zs]) :- N > 0,
     length(Xs,L),
@@ -10,4 +12,4 @@ randomly_select(Xs,N,[X|Zs]) :- N > 0,
     remove(X,Xs,I,Ys),
     N1 is N - 1,
     randomly_select(Ys,N1,Zs).
---
+
