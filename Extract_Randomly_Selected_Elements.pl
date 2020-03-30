08381e8bd93ecc80903f15a5bd0814d19321ee6b
dimitrismatsanganis <jimmatsa@hotmail.gr>
2020-03-31 01:04:42 +0300

    Add code's comments.

--- Extract_Randomly_Selected_Elements.pl
+++ Extract_Randomly_Selected_Elements.pl
@@ -1,7 +1,13 @@
 remove(X,[X|Xs],1,Xs).
-% 
+% The first element in the list is number 1. 
 remove(X,[Y|Xs],N,[Y|Ys]) :- N > 1, 
                              N1 is N - 1, remove(X,Xs,N1,Ys). 
+                             
+% Explanation of the rule remove/4;
+% X is the Nth element of the list L.
+% R is the list that remains when the Nth element is removed from L.
+
+
 randomly_select(_,0,[]).
 % randomly_select(L,N,R), the list R contains N randomly selected 
 % items taken from the list L.
