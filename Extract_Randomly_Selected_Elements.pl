0a433cae2c3a42d999c656e623300fddf5aac6a7
dimitrismatsanganis <jimmatsa@hotmail.gr>
2020-03-31 01:07:28 +0300

    Update Extract_Randomly_Selected_Elements.pl

--- Extract_Randomly_Selected_Elements.pl
+++ Extract_Randomly_Selected_Elements.pl
@@ -1,5 +1,6 @@
 remove(X,[X|Xs],1,Xs).
 % The first element in the list is number 1. 
+
 remove(X,[Y|Xs],N,[Y|Ys]) :- N > 1, 
                              N1 is N - 1, remove(X,Xs,N1,Ys). 
                              
@@ -9,9 +10,7 @@ remove(X,[Y|Xs],N,[Y|Ys]) :- N > 1,
 
 
 randomly_select(_,0,[]).
-% Explanation of the rule randomly_select/3;
-% R is a list which contains N randomly selected 
-% items taken from the list L.
+%If the list contains only one element the select this one.
 
 randomly_select(Xs,N,[X|Zs]) :- N > 0,
     length(Xs,L),
@@ -19,4 +18,8 @@ randomly_select(Xs,N,[X|Zs]) :- N > 0,
     remove(X,Xs,I,Ys),
     N1 is N - 1,
     randomly_select(Ys,N1,Zs).
+    
+% Explanation of the rule randomly_select/3;
+% R is a list which contains N randomly selected 
+% items taken from the list L.
 
