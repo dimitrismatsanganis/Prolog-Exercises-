67e343c8aa884c661d1f980c3c28abad640a1d60
dimitrismatsanganis <jimmatsa@hotmail.gr>
2020-04-06 00:59:21 +0300

    Update prime_list.pl

--- prime_list.pl
+++ prime_list.pl
@@ -1,1 +1,12 @@
-Pending...
+isprime(2).
+% 2 is a prime number.
+
+isprime(3).
+% 3 is a prime number.
+
+isprime(P) :- integer(P), P > 3, P mod 2 =\= 0, \+ factor(P,3).
+% P is a prime number.
+
+factor(K,L) :- N mod L =:= 0.
+factor(K,L) :- L * L < K, L2 is L + 2, factor(K,L2).
+% K has an odd factor F >= L.
