670811be5d6d5cb55e0459b9f43f791b377228a3
dimitrismatsanganis <jimmatsa@hotmail.gr>
2020-04-05 19:30:42 +0300

    Determine whether a given integer number is prime number.
    Example:
    ?- is_prime(9).
    Yes

--- Isprime.pl
+++ Isprime.pl
@@ -10,3 +10,7 @@ isprime(P) :- integer(P), P > 3, P mod 2 =\= 0, \+ factor(P,3).
 factor(K,L) :- N mod L =:= 0.
 factor(K,L) :- L * L < K, L2 is L + 2, factor(K,L2).
 % K has an odd factor F >= L.
+
+%_____________________________________________________
+% Determine whether a given integer number is prime. 
+%_____________________________________________________
