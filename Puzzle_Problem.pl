17a996594674539e3f86743048e1f4899166bd2d
dimitrismatsanganis <jimmatsa@hotmail.gr>
2020-05-02 14:04:57 +0300

    Update Puzzle_Problem.pl

--- Puzzle_Problem.pl
+++ Puzzle_Problem.pl
@@ -30,6 +30,7 @@ perm(L, [A | M]) :-
 % perm(L, M) is true only when list M is a permutation of list L. 
 
 remove(A, [A | L], L).
-remove(A, [B | L], [B | M]) :- remove(A, L, M).
+remove(A, [B | L], [B | M]) :-
+                              remove(A, L, M).
 % remove(A, L, M) is true only when A is a member of list L and M is the
 % residue when A is removed from L.
