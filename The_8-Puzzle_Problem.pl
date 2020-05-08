9ab5c923fea88dc446ebe103caac8036642b6bf9
dimitrismatsanganis <jimmatsa@hotmail.gr>
2020-05-09 00:29:57 +0300

    Update The_8-Puzzle_Problem.pl

--- The_8-Puzzle_Problem.pl
+++ The_8-Puzzle_Problem.pl
@@ -10,9 +10,14 @@ initial_state([2,4,3,1,e,5,7,8,6]).
 goal([1,2,3,4,5,6,7,8,e]).
 
 operator(P,NP):-
-	trans(TempP,P),
-	member((X,Y,e),TempP),
-	move(X,Y,X1,Y1),
-	member((X1,Y1,T),TempP),
-	change(X,Y,X1,Y1,T,TempP,NewTempP),
-	trans(NewTempP,NP).
+		trans(TempP,P),
+		member((X,Y,e),TempP),
+		move(X,Y,X1,Y1),
+		member((X1,Y1,T),TempP),
+		change(X,Y,X1,Y1,T,TempP,NewTempP),
+		trans(NewTempP,NP).
+
+trans( 	[(1,1,T1),(1,2,T2),(1,3,T3),
+	(2,1,T4),(2,2,T5),(2,3,T6),
+	(3,1,T7),(3,2,T8),(3,3,T9)],
+	[T1,T2,T3,T4,T5,T6,T7,T8,T9]).
