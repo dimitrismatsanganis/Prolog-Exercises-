94bff65e077a6f1068a7368df18151414c67259d
dimitrismatsanganis <jimmatsa@hotmail.gr>
2020-05-09 00:28:54 +0300

    Update The_8-Puzzle_Problem.pl

--- The_8-Puzzle_Problem.pl
+++ The_8-Puzzle_Problem.pl
@@ -1,3 +1,16 @@
 /*-------------------------------------------------
    The 8-Puzzle problem
 -------------------------------------------------*/
+
+initial_state([2,4,3,1,e,5,7,8,6]).
+%initial_state([2,3,6,1,5,4,7,e,8]).
+
+goal([1,2,3,4,5,6,7,8,e]).
+
+operator(P,NP):-
+	trans(TempP,P),
+	member((X,Y,e),TempP),
+	move(X,Y,X1,Y1),
+	member((X1,Y1,T),TempP),
+	change(X,Y,X1,Y1,T,TempP,NewTempP),
+	trans(NewTempP,NP).
