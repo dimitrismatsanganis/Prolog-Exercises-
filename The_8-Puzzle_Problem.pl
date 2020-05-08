1eeaa5be35c55584f2d2d66b44b21a0f470d953e
dimitrismatsanganis <jimmatsa@hotmail.gr>
2020-05-09 00:30:30 +0300

    Add move/4 rule.

--- The_8-Puzzle_Problem.pl
+++ The_8-Puzzle_Problem.pl
@@ -21,3 +21,12 @@ trans( 	[(1,1,T1),(1,2,T2),(1,3,T3),
 	(2,1,T4),(2,2,T5),(2,3,T6),
 	(3,1,T7),(3,2,T8),(3,3,T9)],
 	[T1,T2,T3,T4,T5,T6,T7,T8,T9]).
+
+move(X,Y,X,NY):-NY is Y-1,NY>0.	
+% down
+move(X,Y,X,NY):-NY is Y+1,NY<4.	
+% up
+move(X,Y,NX,Y):-NX is X-1,NX>0.	
+% left
+move(X,Y,NX,Y):-NX is X+1,NX<4.	
+% right
